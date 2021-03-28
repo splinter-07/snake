af57f9cc0eaf4492535da0df96d2b3f31b63e99f
splinter-07 <splinter_07@list.ru>
2021-03-28 11:30:12 +0300

    Change

---
+++ SnakeFrame/SnakeFrame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeFrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(120,40);
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
